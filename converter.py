41979bc2a53bf43c6abb5ed14004abd84005bfcb
Joseph Schiarizzi <jschiarizzi@gmail.com>
2018-02-02 06:53:46 -0800

    finish

--- converter.py
+++ converter.py
@@ -1,17 +1,27 @@
 import sys
 import json
 
+#use: $ python3 converter.py sample.txt
+#will create sample.txt.json
+
 txtpath = sys.argv[1]
 txtfile = open(txtpath, "r")
 lines = txtfile.readlines()
-
-jsonpath = txtpath + ".json"
-jsonfile = open(jsonpath, "w")
 
 data = {"perimeter":[]}
 #data["perimeter"].append({"x":1, "y":2})
 
 for line in lines:
-    print(line)
+    x,y = line.split(",")
 
-print(data)
+    #remove '(' and ')\n'
+    x = x[1:]
+    y = y[:-2]
+
+    #add to
+    data["perimeter"].append({"x":float(x), "y":float(y)})
+
+
+jsonpath = txtpath + ".json"
+jsonfile = open(jsonpath, "w")
+json.dump(data, jsonfile)
